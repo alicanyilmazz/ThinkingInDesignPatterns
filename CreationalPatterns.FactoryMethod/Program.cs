6fbc28db88382139afba4e7e17f876655e1438ab
alican yılmaz <alicanyilmaz101@gmail.com>
2026-08-01 17:23:21 +0300

    init

---
+++ CreationalPatterns.FactoryMethod/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualBasic;
+
+Console.WriteLine("Hello, World!");
+ICommissionStrategy strategy;
+
+if (cardType == CardType.Visa)
+{
+    strategy = new VisaCommissionStrategy();
+}
+else if (cardType == CardType.MasterCard)
+{
+    strategy = new MasterCardCommissionStrategy();
+}
+else
+{
+    strategy = new TroyCommissionStrategy();
+}
