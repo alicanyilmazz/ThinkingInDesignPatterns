963e285683eb9565f5efd35b112f732b6c21615c
alican yılmaz <alicanyilmaz101@gmail.com>
2026-08-02 11:24:16 +0300

    init

---
+++ Behavioral.Patterns.Observer/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Hello, World!");
+
+// Observer olmadan problem
+
+/*
+ 
+public sealed class OrderService
+{
+    private readonly EmailService _emailService;
+    private readonly StockService _stockService;
+    private readonly InvoiceService _invoiceService;
+
+    public OrderService(
+        EmailService emailService,
+        StockService stockService,
+        InvoiceService invoiceService)
+    {
+        _emailService = emailService;
+        _stockService = stockService;
+        _invoiceService = invoiceService;
+    }
+
+    public void CreateOrder(Order order)
+    {
+        Console.WriteLine("Sipariş oluşturuldu.");
+
+        _emailService.Send(order);
+        _stockService.Reduce(order);
+        _invoiceService.Create(order);
+    }
+}
+
+*/
